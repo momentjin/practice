025788f93f690b0a52126cc4a1adf293237d1bac
Jaeeon Jin <momentjin.dev@gmail.com>
2024-01-07 19:50:33 +0900

    Create a project for practice

---
+++ src/test/kotlin/com/momentjin/practice/PracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.momentjin.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
